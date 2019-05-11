959cf112c3d464207d8c1c06a42bb1c979d3821c
AishahTufail <ashawan0017@gmail.com>
2019-05-11 18:56:15 +0500

    java file updated

--- app/src/main/java/com/example/labassignment_2/Dialogue.java
+++ app/src/main/java/com/example/labassignment_2/Dialogue.java
@@ -21,51 +21,7 @@ public class Dialogue extends AppCompatActivity {
 
         loadTxt = findViewById(R.id.loadTxt);
 
-    }
-
-    @Override
-    public void onResume(){
-        super.onResume();
-
-        handler = new Handler(){
-            @Override
-            public void handleMessage(Message msg){
-                super.handleMessage(msg);
-                try{
-                    if(msg.what == myThread.THREAD1){
-                        t1 = new myThread(myThread.THREAD1,handler);
-                        new Thread(t1).start();
-                    }
-                    else if(msg.what == myThread.UPDATE){
-                        if(msg.arg1 == myThread.THREAD1){
-                            loadTxt.setText(String.valueOf(msg.arg2));
-                        }
-                    }
-                }
-                catch (Exception e){
-                    Log.d("myTag",e.toString());
-                }
-
-            }
-        };
-
-        startThread();
-
-    }
-
-    public void startThread(){
-        Message msg = new Message();
-        msg.what = myThread.THREAD1;
-        handler.sendMessage(msg);
-    }
+}
 
 
-    @Override
-    public void onDestroy(){
-        String Text = MainActivity.log.getText().toString();
-        MainActivity.log.setText(Text + "Loading Stopped at: " +loadTxt.getText().toString() + "\n");
-        super.onDestroy();
-
-
-    }
 }
